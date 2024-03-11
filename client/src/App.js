a679dd5d839759c216f29b00fd7c47a96a354b78
TDungx2k3 <dungpgpma2k3@gmail.com>
2024-03-11 21:29:33 +0700

    add slider

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import { publicRoutes, adminRoutes, customerRoutes } from "./routes";
 import Footer from "./conponents/Footer/Footer";
 import "./App.css";
+import Slider from "./conponents/Slider/Slider";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           element={
             <div>
               <Header />
+              <Slider />
               <Routes>
                 {publicRoutes.map((route, i) => (
                   <Route key={i} path={route.path} element={<route.page />} />
