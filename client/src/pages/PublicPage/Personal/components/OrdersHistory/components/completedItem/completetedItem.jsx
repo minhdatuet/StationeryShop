b427b75a0febc5bcc7ab480b317a8a8df45dd71e
TDungx2k3 <dungpgpma2k3@gmail.com>
2024-04-26 18:21:47 +0700

    base History

---
+++ client/src/pages/PublicPage/Personal/components/OrdersHistory/components/completedItem/completetedItem.jsx
@@ -0,0 +1,14 @@
+
+import style from "./completedItem.module.scss";
+import clsx from 'clsx';
+
+export const CompletedItem = (props) => {
+    
+    return (
+        <div className= {clsx(style.container)}>
+            
+        </div>
+    )
+};
+
+export default CompletedItem;
