977f8b3cb2adb89ab05447bb0541190f5fa9ef41
TDungx2k3 <dungpgpma2k3@gmail.com>
2024-04-26 22:50:28 +0700

    add order && product in order

---
+++ server/src/models/product_in_order.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductInOrder extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+    //   Order.belongsTo(models.Product, { foreignKey: 'productId', onDelete: 'CASCADE' }),
+      ProductInOrder.belongsTo(models.Order, {foreignKey: 'orderId'}),
+      ProductInOrder.hasOne(models.Product, { foreignKey: 'id', sourceKey: 'productId'})
+    }
+  }
+  ProductInOrder.init({
+    orderId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Product_In_Order',
+  });
+  return ProductInOrder;
+};
