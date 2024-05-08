e43e776efe36b9825edc417dbfe6fb9cb766d100
TDungx2k3 <dungpgpma2k3@gmail.com>
2024-05-08 23:51:51 +0700

    Complete Personal

--- client/src/pages/PublicPage/Personal/components/OrdersHistory/components/completedItem/completedItem.jsx
+++ client/src/pages/PublicPage/Personal/components/OrdersHistory/components/completedItem/completedItem.jsx
@@ -3,29 +3,39 @@ import style from "./completedItem.module.scss";
 import clsx from 'clsx';
 
 export const CompletedItem = (props) => {
-    
+    // console.log(props.data.isRated === 1);
+    const formattedDate = new Date(props.data.purchaseTime).toLocaleString('en-US', {
+    timeZone: 'UTC',
+    hour12: false,
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+    });
     return (
         <div className= {clsx(style.container)}>
             <div className={clsx(style.itemImg)}>
-                <img src={props.data.Product.productImage} alt="" />
+                <img src={props.data.Product_In_Order.Product.productImage} alt="" />
             </div>
 
             <div className={clsx(style.itemInfo)}>
                 <div className={clsx(style.itemName)}>
-                    <p>Name: {props.data.Product.productName}</p>
+                    <p>Name: {props.data.Product_In_Order.Product.productName}</p>
                 </div>
 
                 <div className={clsx(style.purchaseTime)}>
-                    <p>Purchase Time: {props.data.quantity}</p>
+                    <p>Purchase Time: {formattedDate}</p>
                 </div>
             </div>
 
             <div className={clsx(style.viewFeedback)}>
-                <div className={clsx(style.viewFeedbackBtn)}>
+                <div className={clsx(style.viewFeedbackBtn, {[style.hidden]: props.data.isRated === 0})}>
                     View My Feedback
                 </div>
 
-                <div className={clsx(style.giveFeedbackBtn)}>
+                <div className={clsx(style.giveFeedbackBtn, {[style.hidden]: props.data.isRated === 1})}>
                     Give My Feedback
                 </div>
             </div>
