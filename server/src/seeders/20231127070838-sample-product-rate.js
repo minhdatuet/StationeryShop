873b4063d48eac7fd5a5984444f8671a5b61a7f9
TDungx2k3 <dungpgpma2k3@gmail.com>
2024-05-11 17:57:57 +0700

    comment and fix time

--- server/src/seeders/20231127070838-sample-product-rate.js
+++ server/src/seeders/20231127070838-sample-product-rate.js
@@ -1,44 +1,44 @@
-'use strict';
+// 'use strict';
 
-// Function to generate a random 9-digit phone number starting with 0 and with the second digit as '9', '8', or '1'
-function generateRandomPhoneNumber() {
-  const secondDigitOptions = ['9', '8', '1'];
-  const secondDigit = secondDigitOptions[Math.floor(Math.random() * secondDigitOptions.length)];
+// // Function to generate a random 9-digit phone number starting with 0 and with the second digit as '9', '8', or '1'
+// function generateRandomPhoneNumber() {
+//   const secondDigitOptions = ['9', '8', '1'];
+//   const secondDigit = secondDigitOptions[Math.floor(Math.random() * secondDigitOptions.length)];
 
-  const randomNumber = Math.floor(10000000 + Math.random() * 90000000);
-  return '0' + secondDigit + String(randomNumber).substring(0, 8);
-}
+//   const randomNumber = Math.floor(10000000 + Math.random() * 90000000);
+//   return '0' + secondDigit + String(randomNumber).substring(0, 8);
+// }
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Product_Rates', [
-      // {
-      //   // Sample data for the first record
-      //   accountId: 1,
-      //   productId: 1,
-      //   productInOrder: 1,
-      //   rateScore: 3,
-      //   productFeedback: "Normal product"
-      // },
-      // {
-      //   // Sample data for the first record
-      //   accountId: 1,
-      //   productId: 1,
-      //   rateScore: 4,
-      //   productFeedback: "Good product"
-      // },
-      // {
-      //   // Sample data for the first record
-      //   accountId: 1,
-      //   productId: 1,
-      //   rateScore: 5,
-      //   productFeedback: "Best product"
-      // }
-    ])
-},
-    down: async (queryInterface, Sequelize) => {
-        // Remove the seeded data
-        await queryInterface.bulkDelete('Product_Rates', null, {});
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     await queryInterface.bulkInsert('Product_Rates', [
+//       {
+//         // Sample data for the first record
+//         accountId: 1,
+//         productId: 1,
+//         productInOrder: 1,
+//         rateScore: 3,
+//         productFeedback: "Normal product"
+//       },
+//       // {
+//       //   // Sample data for the first record
+//       //   accountId: 1,
+//       //   productId: 1,
+//       //   rateScore: 4,
+//       //   productFeedback: "Good product"
+//       // },
+//       // {
+//       //   // Sample data for the first record
+//       //   accountId: 1,
+//       //   productId: 1,
+//       //   rateScore: 5,
+//       //   productFeedback: "Best product"
+//       // }
+//     ])
+// },
+//     down: async (queryInterface, Sequelize) => {
+//         // Remove the seeded data
+//         await queryInterface.bulkDelete('Product_Rates', null, {});
 
-      },
-};
+//       },
+// };
