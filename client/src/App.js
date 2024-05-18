baba611d39fe7c777d0f5e926533f6513c94ee30
iamduc3024 <iamduc3024@gmail.com>
2024-05-18 17:30:32 +0700

    base login with Google

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,8 @@ import "./App.css";
 import 'aos/dist/aos.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import AboutUs from "./pages/PublicPage/AboutUs/AboutUs.jsx";
-import Policy from "./pages/PublicPage/Policy/Policy.jsx";
 import Slider from "./components/Slider/Slider";
 import Products from "./pages/PublicPage/Products/Products.jsx";
-import AdminPage from "./pages/PrivatePage/AdminPage.jsx";
 
 function App() {
   AOS.init();
@@ -25,27 +22,25 @@ function App() {
 
   return (
     <Routes>
-        <Route
-          path="*"
-          element={
-            <div>
-              <Header />
-              {/* <Products /> */}
-              <Slider />
-              <Routes>
-                {publicRoutes.map((route, i) => (
-                  <Route key={i} path={route.path} element={<route.page />} />
-                ))}
-              </Routes>
-              <Footer />
-            </div>
-          }
-        />
-
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/policy" element={<Policy />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/admin-page" element={<AdminPage />}/>
+      <Route
+        path="*"
+        element={
+          <div>
+            <Header />
+            {/* <Products /> */}
+            <Slider />
+            <Routes>
+              {publicRoutes.map((route, i) => (
+                <Route key={i} path={route.path} element={<route.page />} />
+              ))},
+              {adminRoutes.map((route, i) => (
+                <Route key={i} path={route.path} element={<route.page />} />
+              ))}
+            </Routes>
+            <Footer />
+          </div>
+        }
+      />
     </Routes>
   );
 }
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -9,57 +9,62 @@ import Cart from "../components/Cart/Cart";
 import Products from "../pages/PublicPage/Products/Products";
 import Personal from "../pages/PublicPage/Personal/Personal";
 import Payment from "../components/Payment/Payment";
+import AdminPage from "../pages/PrivatePage/AdminPage";
+
 const publicRoutes = [
-    {
-      path: 'login',
-      page: Login
-    },
-    {
-      path: '/register',
-      page: Register
-    },
-    {
-      path: '/instruction',
-      page: Instruction
-    },
-    {
-      path: '/aboutUs',
-      page: AboutUs
-    },
-    {
-      path: '/product',
-      page: Product
-    },
-    {
-      path: '/cart',
-      page: ShoppingCart
-    },
-    {
-      path: '/',
-      page: Home
-    },
-    {
-      path: '*',
-      page: Home
-    }, 
-    {
-      path: 'products',
-      page: Products
-    },
-    {
-      path: 'personal',
-      page: Personal
-    },
-    {
-      path: 'payment',
-      page: Payment
-    }
-  ];
+  {
+    path: 'login',
+    page: Login
+  },
+  {
+    path: '/register',
+    page: Register
+  },
+  {
+    path: '/instruction',
+    page: Instruction
+  },
+  {
+    path: '/aboutUs',
+    page: AboutUs
+  },
+  {
+    path: '/product',
+    page: Product
+  },
+  {
+    path: '/cart',
+    page: ShoppingCart
+  },
+  {
+    path: '/',
+    page: Home
+  },
+  {
+    path: '*',
+    page: Home
+  },
+  {
+    path: 'products',
+    page: Products
+  },
+  {
+    path: 'personal',
+    page: Personal
+  },
+  {
+    path: 'payment',
+    page: Payment
+  }
+];
 const adminRoutes = [
-    
+  {
+    path: 'admin',
+    page: AdminPage
+  }
 ]
 const customerRoutes = [
 
 ]
 
-export {publicRoutes, adminRoutes, customerRoutes}
+export { publicRoutes, adminRoutes, customerRoutes }
